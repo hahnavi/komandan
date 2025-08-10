2a241f5ec7c76b233b30fbfbc4f813f11e393c05
amunifhanafi <amunifhanafi@gmail.com>
2025-08-10 15:22:09 +0000

    chore(idx): add docker and recommended extensions

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -15,10 +15,18 @@
 
   # Sets environment variables in the workspace
   env = {};
+  services = {
+    docker = {
+      enable = true;
+    };
+  };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+      "fill-labs.dependi"
+      "kilocode.kilo-code"
+      "rust-lang.rust-analyzer"
+      "tamasfe.even-better-toml"
     ];
 
     # Workspace lifecycle hooks
